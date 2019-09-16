206fde2c576efd7731c8aacad1d5ebab64e23c53
Reed Oei <oei.reed@gmail.com>
2019-09-16 12:07:43 -0500

    Fix curl invocation

--- install-spot.sh
+++ install-spot.sh
@@ -8,7 +8,7 @@ date
 SPOT_VERSION="2.8.1"
 
 # download/uncompress spot
-curl "http://www.lrde.epita.fr/dload/spot/spot-$SPOT_VERSION.tar.gz" -O "spot-$SPOT_VERSION.tar.gz"
+curl -o "spot-$SPOT_VERSION.tar.gz" "http://www.lrde.epita.fr/dload/spot/spot-$SPOT_VERSION.tar.gz"
 gunzip "spot-$SPOT_VERSION.tar.gz"
 tar xvf "spot-$SPOT_VERSION.tar"
 cd "spot-$SPOT_VERSION"
