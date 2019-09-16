d8a950030f17a5d03cb6b39851d923e82eb1bf96
Reed Oei <oei.reed@gmail.com>
2019-09-16 10:15:39 -0500

    Added travis build and basic structure

---
+++ install-spot.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+set -ex
+
+echo "$(git rev-parse HEAD)"
+date
+
+SPOT_VERSION="2.8.1"
+
+# download/uncompress spot
+wget "http://www.lrde.epita.fr/dload/spot/spot-$SPOT_VERSION.tar.gz"
+gunzip "spot-$SPOT_VERSION.tar.gz"
+tar xvf "spot-$SPOT_VERSION.tar"
+cd "spot-$SPOT_VERSION"
+
+./configure --prefix ~/usr
+make
+make install
+
---
+++ pecan/pecan.py
@@ -0,0 +1,8 @@
+import sys
+
+def main(args):
+    pass
+
+if __name__ == '__main__':
+    main(sys.argv)
+
