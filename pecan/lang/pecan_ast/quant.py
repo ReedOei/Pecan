501a17b72b062203bc2424b6b8ba07e936910a29
Reed Oei <oei.reed@gmail.com>
2019-10-16 13:25:35 -0500

    Code cleanup

--- pecan/lang/pecan_ast/quant.py
+++ pecan/lang/pecan_ast/quant.py
@@ -16,11 +16,6 @@ class Forall(Predicate):
         self.pred = pred
 
     def evaluate(self, prog):
-        # evaluated = self.pred.evaluate(prog)
-        # for e in evaluated.edges():
-        #     v = buddy.bdd_ithvar(evaluated.register_ap(self.var_name))
-        #     e.cond = (buddy.bdd_not(v) | e.cond) & (v | e.cond) # (y & x) & (!x & (x | y))
-        # return spot.minimize_obligation(evaluated)
         return Complement(Exists(self.var_name, Complement(self.pred))).evaluate(prog)
 
     def __repr__(self):
@@ -48,11 +43,15 @@ class Exists(Predicate):
         new_aut.set_init_state(0)
 
         for e in evaluated.edges():
+            # Convert to a formula because formulas are nicer to work with than the bdd's
             formula = spot.formula(spot.bdd_format_formula(evaluated.get_dict(), e.cond))
 
             if_0 = self.substitute({self.var_name: spot.formula('0')}, formula)
             if_1 = self.substitute({self.var_name: spot.formula('1')}, formula)
 
+            # The new edge condition should be:
+            # [0/y]cond | [1/y]cond
+            # where cond is the original condition. That is, the edge is taken if it holds with y being false or y being true.
             cond = self.create_edge_condition(evaluated, if_0) | self.create_edge_condition(evaluated, if_1)
 
             new_aut.new_edge(e.src, e.dst, cond, e.acc)
@@ -67,54 +66,6 @@ class Exists(Predicate):
                 return formula
         else:
             return formula.map(lambda x: self.substitute(subs, x))
-
-    def ensure_formula_valid(self, formula):
-        if formula is None:
-            return spot.formula_tt()
-        else:
-            return formula
-
-    def remove_var(self, formula):
-        if formula._is(spot.op_ap): # op_ap is 'atomic proposition' (i.e., variable)
-            if formula.ap_name() == self.var_name:
-                return None
-            else:
-                return formula
-        else:
-            new_children = []
-            for i in range(formula.size()):
-                new_child = self.remove_var(formula[i])
-
-                if new_child is not None:
-                    new_children.append(new_child)
-
-            return self.rebuild_formula(formula.kind(), new_children)
-
-    def rebuild_binary(self, constr, children):
-        if len(children) == 0:
-            return None
-        elif len(children) == 1:
-            return children[0]
-        else:
-            return constr(children)
-
-    def rebuild_formula(self, kind, new_children):
-        if kind == spot.op_And:
-            return self.rebuild_binary(spot.formula_And, new_children)
-        elif kind == spot.op_Or:
-            return self.rebuild_binary(spot.formula_Or, new_children)
-        elif kind == spot.op_Not:
-            if len(new_children) == 0:
-                return None
-            else:
-                return spot.formula_Not(new_children[0])
-        elif kind == spot.op_tt:
-            return spot.formula_tt()
-        elif kind == spot.op_ff:
-            return spot.formula_ff()
-        else:
-            raise Exception('Unhandled formula kind: {}'.format(kind))
-
 
     def create_edge_condition(self, bdd_dict, new_formula):
         return FormulaToBdd(bdd_dict, new_formula).translate()
