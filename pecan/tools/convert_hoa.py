73aa3ba8e136d66238d495a04b24c930f3645269
Unknown <tatuminabook@gmail.com>
2019-10-30 05:50:35 -0500

    adds intermediate states for non-binary inputs

--- pecan/tools/convert_hoa.py
+++ pecan/tools/convert_hoa.py
@@ -1,5 +1,14 @@
 import math
 
+# GLOBAL VARIABLES
+state_counter = 0
+states = dict()
+curr_state = -1
+base = 2
+org_states = []
+
+
+# MISC HELPER FUNCTIONS
 def first_word(string):
         fw = ""
         while len(string) and string[0] != ' ':
@@ -8,26 +17,75 @@ def first_word(string):
         string = string[1:]
         return (fw, string)
 
-def state(old_line):
-        new_line = 'State: '
-        old_line = old_line[:len(old_line) - 1]
-        (temp, old_line) = first_word(old_line) 
-        new_line += temp
-        return (new_line + ' { ' + first_word(old_line)[0] + ' }\n', temp)
-        
-def edge(old_line, bases):
-        new_line = '['
-        i = 0
-        while old_line[0] != '-':
-            (temp, old_line) = first_word(old_line) 
-            if bases != []:
-                binary = '0' + str(bases[i]) + 'b'
-                i += 1
-                temp = '&'.join([i for i in format(int(temp), binary)])
-            new_line += temp + '&'
-        new_line = new_line[:len(new_line) - 1] + '] '
-        return new_line + old_line[3:]
+def column(l, c):
+        column = ""
+        for r in l:
+            column += r[c]
+        return column
 
+def reset_global():
+        global state_counter
+        state_counter = 0
+        global intermediate_states
+        intermediate_states = []
+        org_states = []
+
+
+# STATE FUNCTIONS
+def state_line(line):
+        line = line[:len(line) - 1]
+        acc = first_word(line)[1]
+        global state_counter, curr_state
+        curr_state = state_counter + 1
+        new_line = state(acc)
+        global org_states
+        org_states.append(int(new_line[8:-7]))
+        return new_line
+        
+def state(acc):
+        global intermediate_states
+        line = 'States: '
+        global state_counter
+        state_counter += 1
+        line += str(state_counter)
+        line += ' { ' + acc + ' }\n'
+        state = {'print': line}
+        global states
+        states[str(state_counter)] = state
+        return line
+
+
+# EDGE FUNCTIONS
+def intermediate_edge(b_inputs, start_state, end_state, base): 
+        global states
+        s = states[str(start_state)]
+        for i in range(base):
+            inp = column(b_inputs, i)
+            if inp in s.keys():
+                s = states[s[inp]]
+            elif i == base - 1:
+                s[inp] = 'o' + str(end_state)
+            else:
+                new_state = state('0')
+                name = first_word(new_state[8:])[0]
+                s[inp] = name
+                states[name] = {'print': new_state}
+                s = states[name]
+
+def edge(old_line, base):
+        inputs = []
+        while old_line[0] != '-':
+            (inp, old_line) = first_word(old_line)
+            inputs.append(('{0:b}'.format(int(inp))).zfill(base))
+        end_state = first_word(old_line[3:-1])[0]
+        global curr_state
+        intermediate_edge(inputs, curr_state, end_state, base)
+
+def print_edges(bin_str, state):
+        return '[' + '&'.join(bin_str) + '] ' + str(state) + '\n'
+
+
+# BASE FUNCTIONS 
 def get_inputs(edge):
         counter = 0
         for l in edge:
@@ -50,41 +108,51 @@ def adjust_base(line):
                 base = 1
         return bases
 
+
+# CONVERT FUNCTIONS
 def convert_hoa(txt1, txt2):
         f1 = open(txt1, 'r')
         f2 = open(txt2, 'w')
 
         body = ['--BODY--\n']
         
-        inputs = -1
-        states = 0
         bases = []
-        
+        base = 0
+
         for line in f1.readlines():
             if line[0] == '{':
                 bases = adjust_base(line)
                 for i in range(len(bases)):
-                    bases[i] = math.ceil(math.log(bases[i] + 1, 2))
+                    bases[i] = math.ceil(math.log(bases[i], 2))
+                base = max(bases)
             elif '->' in line:
-                body.append(edge(line, bases))
-                if inputs == -1:
-                    inputs = get_inputs(line) 
+                edge(line, base)
             elif len(line) > 0:
-                (temp, states) = state(line)
-                body.append(temp)
+                state_line(line)
+
+        for i in states.keys():
+            curr_state = states[i]
+            body.append(curr_state['print'])
+            del curr_state['print']
+            for j in curr_state.keys():
+                end_state = curr_state[j]
+                if end_state[0] == 'o':
+                    end_state = org_states[int(end_state[1:])]
+                body.append(print_edges(j, end_state))
+
         body.append('--END--')
-        
-        header = ['HOA: v1\n', 'States: ' + str(states) + '\n', 'acc-name: Buchi\n']
+
+        global state_counter
+        header = ['HOA: v1\n', 'States: ' + str(state_counter) + '\n', 'acc-name: Buchi\n']
         header.append('Acceptance: 1 Inf(0)\n')
         
         aps = ''
-        if bases != []:
-            inputs = 0
-            for base in bases:
-                inputs += base
+        inputs = len(bases)
         for i in range(inputs):
             aps += ' "v' + str(i) + '"'
         header.append('AP: ' + str(inputs) + aps + '\n') 
 
         for line in (header + body):
             f2.write(line)
+        
+        reset_global()
