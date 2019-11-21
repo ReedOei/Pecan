448ca00b5e7754859cf8c2670079492fc154d44f
Reed Oei <oei.reed@gmail.com>
2019-11-21 16:20:41 -0600

    Fix another bug in file loading

--- pecan/program.py
+++ pecan/program.py
@@ -8,7 +8,7 @@ from pecan.lang.pecan_ast import Program
 
 PECAN_PATH_VAR = 'PECAN_PATH'
 
-def make_search_paths(filepath=None):
+def make_search_paths(filename=None):
     own_path = os.path.dirname(os.path.realpath(__file__))
     std_library_path = os.path.join(own_path, '..', 'library')
     automata_library_path = os.path.join(own_path, '..', 'library', 'automata')
@@ -17,16 +17,17 @@ def make_search_paths(filepath=None):
     search_paths = ['.', std_library_path, automata_library_path]
 
     # If we're creating a search path for some file (which is almost always the case), then include the base directory of that file as well
-    if filepath is not None:
-        search_paths.append(os.path.dirname(filepath))
+    if filename is not None:
+        search_paths.append(os.path.dirname(filename))
 
     if PECAN_PATH_VAR in os.environ:
         search_paths.extend(os.getenv(PECAN_PATH_VAR).split(os.pathsep))
 
     return search_paths
 
-def load(filename, *args, **kwargs):
-    with open(filename, 'r') as f:
+def load(pecan_file, *args, **kwargs):
+    with open(pecan_file, 'r') as f:
+        kwargs['filename'] = pecan_file
         return from_source(f.read(), *args, **kwargs)
 
 def from_source(source_code, *args, **kwargs):
@@ -34,7 +35,7 @@ def from_source(source_code, *args, **kwargs):
 
     prog.debug = kwargs.get('debug', False)
     prog.quiet = kwargs.get('quiet', False)
-    prog.search_paths = make_search_paths()
+    prog.search_paths = make_search_paths(kwargs.get('filename', None))
     prog.loader = load
 
     # Load the standard library
@@ -44,12 +45,12 @@ def from_source(source_code, *args, **kwargs):
         kwargs_copy['load_stdlib'] = False
         kwargs_copy['quiet'] = kwargs.get('quiet_stdlib', kwargs.get('quiet', False))
 
-        stdlib_prog = load(prog.locate_file('std.pn'), **kwargs_copy)
+        stdlib_prog = load(prog.locate_file('std.pn'), *args, **kwargs_copy)
         stdlib_prog.evaluate()
         prog.include(stdlib_prog)
 
     if prog.debug:
-        print('Path: {}'.format(prog.search_paths))
+        print('Search path: {}'.format(prog.search_paths))
 
     return prog
 
