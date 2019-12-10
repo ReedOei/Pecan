2ecce024c1f3bb7f1ac4610d3c09f1460410f6c3
Reed Oei <oei.reed@gmail.com>
2019-12-10 14:52:40 -0600

    Make sure to record optimizer changes

--- pecan/lang/optimizer/boolean.py
+++ pecan/lang/optimizer/boolean.py
@@ -10,12 +10,15 @@ class BooleanOptimizer(BasicOptimizer):
     def transform_Complement(self, node):
         if type(node.a) is Complement:
             # !(!P) is equivalent to P
+            self.changed = True
             return self.transform(node.a.a)
 
         # DeMorgan's Laws: Pushing complements down seems to help
         elif type(node.a) is Conjunction:
+            self.changed = True
             return self.transform(Disjunction(Complement(node.a.a), Complement(node.a.b)))
         elif type(node.a) is Disjunction:
+            self.changed = True
             return self.transform(Conjunction(Complement(node.a.a), Complement(node.a.b)))
 
         else:
