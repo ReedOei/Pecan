50863739e57d8884a82deebe0b19075f2f5853a0
Reed Oei <oei.reed@gmail.com>
2020-01-22 14:27:15 -0600

    Improve install spot script

--- install-spot.sh
+++ install-spot.sh
@@ -9,12 +9,22 @@ SPOT_VERSION="2.8.5"
 PYTHON_VERSION="3.6"
 
 # download/uncompress spot
-curl -o "spot-$SPOT_VERSION.tar.gz" "http://www.lrde.epita.fr/dload/spot/spot-$SPOT_VERSION.tar.gz"
-gunzip "spot-$SPOT_VERSION.tar.gz"
-tar xvf "spot-$SPOT_VERSION.tar"
-cd "spot-$SPOT_VERSION"
 
-./configure --prefix ~/.local
-make
-sudo make install
+if ! python3 -c "import spot"; then
+    if [[ ! -d "spot-$SPOT_VERSION" ]]; then
+        curl -o "spot-$SPOT_VERSION.tar.gz" "http://www.lrde.epita.fr/dload/spot/spot-$SPOT_VERSION.tar.gz"
+        gunzip "spot-$SPOT_VERSION.tar.gz"
+        tar xvf "spot-$SPOT_VERSION.tar"
+    else
+        echo "Skipped downloading spot, directory already exists"
+    fi
+
+    cd "spot-$SPOT_VERSION"
+
+    ./configure --prefix ~/.local
+    make
+    sudo make install
+else
+    echo "Skipped installing spot---already found."
+fi
 
