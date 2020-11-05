b6a77f7b6a84c44b214350d0722c24550d69ba3a
Reed Oei <oei.reed@gmail.com>
2020-11-05 16:52:25 -0600

    Fix bug with type inference inferring AnyType

--- pecan/lang/ir/prog.py
+++ pecan/lang/ir/prog.py
@@ -494,7 +494,7 @@ class Program(IRNode):
     def get_restrictions(self, var_name: str):
         result = []
         # for scope in self.restrictions:
-        for r in self.restrictions[-1].get(var_name, []) + self.global_restrictions.get(var_name, []):
+        for r in self.get_restriction_env().get(var_name, []):
             if not r in result:
                 result.append(r)
         return result
